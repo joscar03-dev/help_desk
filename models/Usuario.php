b047d22521250aff35558c4216d9fa4b31742f8f
joscar03-dev <locomancocapac@gmail.com>
2024-10-16 18:38:14 -0500

    modelo usuario

--- models/Usuario.php
+++ models/Usuario.php
@@ -15,10 +15,15 @@ class Usuario extends Conectar
                 header("location:" . $this->ruta() . "index.php?m=2");
                 exit();
             } else {
-                $sql=" SELECT * FROM tm_usuario WHERE usu_correo = '$correo' AND usu_pass = '$password' AND estado=1";
-                $stmt=$conectar->prepare($sql);
-                $stmt->bindValue("1", $correo);
-                $stmt->bindValue("2", $password);
+                // Consulta SQL con placeholders
+                $sql = "SELECT * FROM tm_usuario WHERE usu_correo = :correo AND usu_pass = :password AND estado = 1";
+                $stmt = $conectar->prepare($sql);
+                
+                // Vincula los parámetros con los valores correspondientes
+                $stmt->bindValue(":correo", $correo);
+                $stmt->bindValue(":password", $password);
+                
+                // Ejecuta la consulta
                 $stmt->execute();
                 $resultado = $stmt->fetch();
                 if (is_array( $resultado ) AND count($resultado) > 0) {
