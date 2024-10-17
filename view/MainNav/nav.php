c78c9aa020f1668099d1a486566e3bd8c06a8b5e
joscar03-dev <locomancocapac@gmail.com>
2024-10-17 18:10:47 -0500

    nav

---
+++ view/MainNav/nav.php
@@ -0,0 +1,25 @@
+<nav class="side-menu">
+    <ul class="side-menu-list">
+        <li class="blue-dirty">
+            <a href="../../view/home/">
+                <span class="glyphicon glyphicon-th"></span>
+                <span class="lbl">Inicio</span>
+            </a>
+        </li>
+        <li class="blue-dirty">
+            <a href="../../view/NuevoTicket/">
+                <span class="glyphicon glyphicon-th"></span>
+                <span class="lbl">NuevoTicket</span>
+            </a>
+        </li>
+        <li class="blue-dirty">
+            <a href="../../view/ConsultarTicket/">
+                <span class="glyphicon glyphicon-th"></span>
+                <span class="lbl">consultarticket</span>
+            </a>
+        </li>
+    </ul>
+
+
+
+</nav><!--.side-menu-->
