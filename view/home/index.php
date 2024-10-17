104bc0861bb2e844049b28288b0ddece67403e51
joscar03-dev <locomancocapac@gmail.com>
2024-10-17 18:10:57 -0500

    home

--- view/home/index.php
+++ view/home/index.php
@@ -1,1 +1,47 @@
-<div>HOME</div>
+<?php
+    require_once("../../config/conexion.php");
+    if (isset($_SESSION["usu_id"])){
+?>
+
+<!DOCTYPE html>
+<html>
+    <!-- Head -->
+<?php
+
+require_once("../../view/MainHead/head.php");
+?>
+
+
+<title>StartUI - Premium Bootstrap 4 Admin Dashboard Template</title>
+<body class="with-side-menu">
+
+    <!-- HEADER -->
+    <?php
+        require_once("../../view/MainHeader/header.php");
+    ?>
+
+    <div class="mobile-menu-left-overlay"></div>
+    <!-- SIDEBAR NAV -->
+    <?php
+        require_once("../../view/MainNav/nav.php");
+    ?>
+
+    <div class="page-content">
+        <div class="container-fluid">
+            Blank page.
+        </div><!--.container-fluid-->
+    </div><!--.page-content-->
+
+    <!-- scripts -->
+    <?php
+        require_once("../../view/MainJs/js.php");
+    ?>
+    <script type="text/javascript" src="home.js"></script>
+</body>
+
+</html>
+<?php
+    } else {
+        header("Location:../../index.php");
+    }
+?>
